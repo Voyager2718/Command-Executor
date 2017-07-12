2da656041be0cddb6687f0bd33e9490ea52d3a3e
Voygaer2718 <zhipeng.yang.it@hotmail.com>
2017-07-12 18:54:39 +0800

    Commented compilation options.

--- Settings.h
+++ Settings.h
@@ -1,1 +1,11 @@
+/**
+  * Here's you can modify compilation settings.
+  */
+
+#ifndef SETTINGS
+#define SETTINGS
+
+// Specify the target Operating System. Can be either linux or win32 (Not supported yet).
 #define TARGET_OS linux
+
+#endif
