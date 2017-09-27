8c8bc3938f506ba495f15692e888c23febe962b6
Voygaer2718 <voyager2718@gmail.com>
2017-09-27 15:54:29 +0800

    Added unified report string system

---
+++ src/ReportString/ReportString.cpp
@@ -0,0 +1,58 @@
+#include "ReportString.h"
+
+ReportString &ReportString::GetInstance()
+{
+    static ReportString instance;
+    return instance;
+}
+
+/**
+  * Return 0 if successful, return non 0 otherwise.
+  */
+int ReportString::AddReportString(ReportType type, string reportno, string description)
+{
+    if (reportStrings.find(reportno) == reportStrings.end())
+    {
+        string s_type = "";
+        switch (type)
+        {
+        case FATAL:
+            s_type += "FATAL";
+            break;
+        case WARNING:
+            s_type += "WARNING";
+            break;
+        case INFO:
+            s_type += "INFO";
+            break;
+        case SUCCESS:
+            s_type += "SUCC";
+            break;
+        }
+        reportStrings[reportno] = "[" + s_type + "] " + reportno + ": " + description + " ";
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+string ReportString::GetReportString(string reportno, string fromFile)
+{
+    return (reportStrings[reportno] + "[" + fromFile + "]");
+}
+
+ReportString::ReportString()
+{
+    AddReportString(SUCCESS, "SUCC-0001", "Process execution successful.");
+
+    AddReportString(FATAL, "INIT-0001", "Reached maximum instance restriction.");
+
+    AddReportString(FATAL, "CMD-0001", "Command execution failed.");
+    AddReportString(FATAL, "CMD-0002", "Command not set.");
+
+    AddReportString(FATAL, "EXEC-0001", "Cannot execute command.");
+
+    AddReportString(FATAL, "FORK-0001", "Unable to create new process.");
+}
--- ycemain.cpp
+++ ycemain.cpp
@@ -8,6 +8,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "lib/InstanceRestriction/instance_restriction.h"
+
+#include "src/ReportString/ReportString.h"
 #include "src/Executor/Executor.h"
 #include "src/Runnable/Command/Command.h"
 #include "src/Runnable/IRunnable.h"
@@ -19,63 +22,11 @@ using std::to_string;
 
 int currentInstanceLock;
 
-bool ReachedMaximumInstance()
-{
-    for (int i = 0;; i++)
-    {
-        int fd = open(("/tmp/yce_instance" + to_string(i) + ".lock").c_str(), O_RDONLY);
-        if (fd > 0)
-        {
-            pid_t maybePid;
-            read(fd, &maybePid, sizeof(maybePid));
-
-            if (kill(maybePid, 0) == 0)
-            {
-                if (i >= MAX_INSTANCE - 1)
-                {
-                    cout << "[INFO] INIT-0001: Only " + to_string(MAX_INSTANCE) + " instance can be run at the same time.\nModify maximum instance at Settings.h and recompile. [ycemain]" << endl;
-                    return true;
-                }
-                close(fd);
-            }
-            else if (errno == ESRCH)
-            {
-                remove(("/tmp/yce_instance" + to_string(i) + ".lock").c_str());
-
-                currentInstanceLock = i;
-
-                int fd2 = open(("/tmp/yce_instance" + to_string(i) + ".lock").c_str(), O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP);
-                pid_t currentPid = getpid();
-                write(fd2, &currentPid, sizeof(currentPid));
-
-                return false;
-            }
-            else
-            {
-                perror("[FATAL] INIT-0002: Unknown process status. [ycemain]");
-            }
-        }
-        else
-        {
-            currentInstanceLock = i;
-
-            int fd2 = open(("/tmp/yce_instance" + to_string(i) + ".lock").c_str(), O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP);
-            pid_t currentPid = getpid();
-            write(fd2, &currentPid, sizeof(currentPid));
-
-            return false;
-        }
-    }
-}
-
 int main(int argc, char *argv[])
 {
     if (ReachedMaximumInstance())
     {
-        return 1;
+        (Report::GetInstance()).AddReport((ReportString::GetInstance()).GetReportString("INIT-0001", "ycemain"), FAILED);
+        return 0;
     }
-
-    sleep(5);
-
-    remove(("/tmp/yce_instance" + to_string(currentInstanceLock) + ".lock").c_str());
 }
